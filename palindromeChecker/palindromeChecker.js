09a864d8f81932c8ffb1a67a0402b4959b7027d9
M_Nahuel <91757112+M-Nahuel@users.noreply.github.com>
2023-05-10 22:12:17 -0400

    html added

--- palindromeChecker/palindromeChecker.js
+++ palindromeChecker/palindromeChecker.js
@@ -8,6 +8,10 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
 
+const check = document.getElementById('check');
+const string = document.getElementById('str');
+const result = document.getElementById('result');
+
 function palindrome(str) {
     str=str.toLowerCase().split("")
     let string = str.filter(n=>/[a-z0-9]/.test(n))
@@ -23,13 +27,10 @@ function palindrome(str) {
     
     return palindrome;
   }
-  
-  console.log(palindrome("eye"));
-  console.log(palindrome("_eye"));
-  console.log(palindrome("race car"));
-  console.log(palindrome("not a palindrome"));
-  console.log(palindrome("A man, a plan, a canal. Panama"));
-  console.log(palindrome("never odd or even"));
-  console.log(palindrome("nope"));
-  console.log(palindrome("almostomla"));
-  console.log(palindrome("0_0 (: /-\ :) 0-0"));
+
+check.addEventListener('click', () => {
+  const pal = palindrome(string.value) ?
+  ''
+  : 'not ';
+  result.textContent = `The string provided is ${pal}a palindrome.`;
+});
